696c8d8aa182b5dffa598cc3e83f79aaf9fcc8bd
usertar <ahmedmario@hotmail.es>
2024-10-14 18:13:40 +0200

    nuevos comienzos

---
+++ EjTelnet/src/main/java/EjServiciosRed/EjTelnet/App.java
@@ -0,0 +1,13 @@
+package EjServiciosRed.EjTelnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
